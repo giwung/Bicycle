f95d8f1746c9db9aee1ece5d971ef2d72cdd0aff
khjc <khjc7265@gmail.com>
2017-09-24 19:22:08 +0900

    test activity

--- app/src/main/java/misso/mra/bicycle/MainActivity.kt
+++ app/src/main/java/misso/mra/bicycle/MainActivity.kt
@@ -1,9 +1,18 @@
 package misso.mra.bicycle
 
 import android.app.Activity
+import android.os.Bundle
 
 /**
  * Created by hyeonjung on 2017. 9. 24..
  */
 class MainActivity : Activity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
 }
